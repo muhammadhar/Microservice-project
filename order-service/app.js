b99386ff35a049732ccf9191c18b84769a722dc2
muhammadhar <hariskhan38273@gmail.com>
2026-03-25 13:21:30 +0000

    Add Docker Compose for all services

--- order-service/app.js
+++ order-service/app.js
@@ -7,8 +7,9 @@ app.use(cors());
 
 app.get('/order', async (req, res) => {
     try {
-        const userResp = await axios.get('http://localhost:3010/user');
-        const productResp = await axios.get('http://localhost:3020/product');
+        // Correct routes for Docker network
+        const userResp = await axios.get('http://user-service:3010/user');
+        const productResp = await axios.get('http://product-service:3020/product');
 
         const order = {
             orderId: 5001,
